f270b189c986954ab1011f304b41f96f032dab23
Mohammad Oshaghi <mroshaghi@gmail.com>
2018-05-24 01:47:24 +0530

    Update gns3_install_full.sh

--- gns3_install_full.sh
+++ gns3_install_full.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
-#===================dynamips===========================#
+#=================== Start ===========================#
 # Check if the script is being run by root
 if [ "$(id -u)" != "0" ]; then
 	echo "This scripts must be run as root" 1>&2
 	exit 1
 fi
 
+# you shoud update your fedora manually
 # Update system
-dnf -y update
+# dnf -y update
 
 # Install dnf packages
-dnf -y install gns3-server gns3-gui.noarch gcc cmake elfutils-libelf-devel libuuid-devel libpcap-devel python3-devel redhat-rpm-config python3-qt5 python3-sip glibc-static xterm wget git bison flex docker tigervnc wireshark-qt
+dnf -y install gns3-server gns3-gui.noarch gcc cmake elfutils-libelf-devel libuuid-devel libpcap-devel python3-devel redhat-rpm-config python3-qt5 python3-sip glibc-static xterm wget git bison flex docker tigervnc wireshark-qt elfutils-libelf-devel libpcap-devel glibc-devel.i686 elfutils-devel.i686 libpcap-devel.i686
 
+#======================dynamips========================#
 # Create a temporary folder on /tmp
 mkdir -p /tmp/build-dynamips
 cd /tmp/build-dynamips
 
-# Clone repo from github
 git clone git://github.com/GNS3/dynamips.git
-cd dynamips
+cd dynamips/
 mkdir build
 cd build
-cmake ..
-
-# Install
-make
+cmake .. -DDYNAMIPS_ARCH=x86
 make install
 
 # Remove folder
@@ -46,30 +44,23 @@ cd iouyap
 make
 sudo make install
 
-
 #=======================================#
 cd /tmp
 git clone git://github.com/GNS3/ubridge.git
 cd ubridge
 make
 make install
-#/usr/local/bin/ubridge
-
+ls -lash /usr/local/bin/ubridge
 
 #=====================vpcs==================#
 
 dnf copr enable athmane/gns3-extra -y
-dnf install vpcs dynamips -y
-
+dnf install vpcs -y
 
 #=====================display to user==================#
 
-
-
-echo " GNS3 Installation script finishe          "
+echo " GNS3 Installation script finished          "
 echo " VPCS Install directory: /opt/vpcs/src/vpcs     "
-
-
 echo " newgrp docker "
 echo " groupadd docker && sudo gpasswd -a ${USER} docker && sudo systemctl restart docker"
 echo " sudo usermod -a -G wireshark ${USER} "
